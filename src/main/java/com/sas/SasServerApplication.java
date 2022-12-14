c7e6128da58c2b5fae05366c2e0c67bb0c246e55
Rohit Bavkar <rohitbavkar95@gmail.com>
2022-12-15 01:44:20 +0530

    test

---
+++ src/main/java/com/sas/SasServerApplication.java
@@ -0,0 +1,13 @@
+package com.sas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SasServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SasServerApplication.class, args);
+    }
+
+}
