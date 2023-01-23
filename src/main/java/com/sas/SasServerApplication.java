e8527758ed89de0a91ac1f48e9759bac2ccb04df
Rohit Bvakar <rohitbavkar95@gmail.com>
2023-01-23 21:08:42 +0530

    Spring IOC Container

--- src/main/java/com/sas/SasServerApplication.java
+++ src/main/java/com/sas/SasServerApplication.java
@@ -2,14 +2,12 @@ package com.sas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/**
- * @author rohit
- **/
+
 @SpringBootApplication
 public class SasServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SasServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SasServerApplication.class, args);
+	}
 
 }
